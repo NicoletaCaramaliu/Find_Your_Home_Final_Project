bcf385666dcbdc3a50a89125f7f00aade9377edb
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2025-01-26 03:20:26 +0200

    Add dto response and request for user and modify the name for password in user class

--- Find_Your_Home/Models/User/User.cs
+++ Find_Your_Home/Models/User/User.cs
@@ -13,7 +13,7 @@ namespace Find_Your_Home.Models.User
     {
         public string Email { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
         public Role Role { get; set; } = Role.User;
 
 
---
+++ Find_Your_Home/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Find_Your_Home.Data;
+using Find_Your_Home.Models.User;
+using Find_Your_Home.Repositories.GenericRepository;
+
+namespace Find_Your_Home.Repositories.UserRepository
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            return await FindSingleOrDefaultAsync(user => user.Email == email);
+        }
+    }
+}
