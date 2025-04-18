143cafc1e72f68c784b73af41852c3e0ca8a2282
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2025-04-19 00:27:29 +0300

    log test

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { refreshToken } from "./services/authService";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL ;
 
 
 interface RetryAxiosRequestConfig extends AxiosRequestConfig {
